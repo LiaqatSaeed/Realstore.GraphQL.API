2d87aa2e607a61c981b62005300c4efc58dd9423
Liaqat Saeed <liaqatsaeed007@gmail.com>
2021-09-16 22:18:11 +0500

    Simple Appolo server setup completed

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { connectDB } from "./db/connect.js";
+import { ApolloServer, gql } from "apollo-server-express";
 
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+await server.start();
 const app = express();
+
+server.applyMiddleware({ app });
+
 app.use(bodyParser.json());
 app.use("*", cors());
 
 app.listen({ port: 4000 }, () => {
   connectDB();
-  console.log(`🚀 Server ready at http://localhost:4000`);
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
 });
